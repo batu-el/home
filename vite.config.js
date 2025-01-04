89b96e06cf0b0333abdb681f212356c7f44a3c13
batu-el <bel@tulane.edu>
2025-01-04 14:59:06 -0800

    fixing vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',  // Changed from '/home/' to './'
+  base: '/home/',  // Changed from '/home/' to './'
   plugins: [react()]
 })
