893da81394a81891380944091796e54f24b08de3
batu-el <bel@tulane.edu>
2025-01-04 14:39:48 -0800

    Setup for GitHub Pages deployment

--- vite.config.cs
+++ vite.config.cs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/home/',  // Changed to match the repository name
+  base: "/home/",
+  plugins: [react()]
 })
+
